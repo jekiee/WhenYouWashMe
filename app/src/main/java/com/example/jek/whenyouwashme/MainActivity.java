59a7bc435dd0cc58cdce240db961223b0ce786cc
eugene <yauhenihuk@gmail.com>
2017-05-25 20:04:24 +0300

    3-d commit

--- app/src/main/java/com/example/jek/whenyouwashme/MainActivity.java
+++ app/src/main/java/com/example/jek/whenyouwashme/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.jek.whenyouwashme;
 
+import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -8,6 +9,12 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            setContentView(R.layout.activity_main_landscape);
+        } else {
+            setContentView(R.layout.activity_main);
+        }
+
+
     }
 }
