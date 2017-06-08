8bf5f9a659cbd82cd2cac0d5ac32ffa5aface4db
eugene <yauhenihuk@gmail.com>
2017-06-08 21:44:02 +0300

    1st restoring

--- app/src/main/java/com/example/jek/whenyouwashme/activity/MainActivity.java
+++ app/src/main/java/com/example/jek/whenyouwashme/activity/MainActivity.java
@@ -6,12 +6,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.jek.whenyouwashme.R;
+import com.google.firebase.FirebaseApp;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        /*FirebaseApp.initializeApp(this);*/
         if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
             setContentView(R.layout.activity_main_landscape);
         } else {
@@ -20,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
     @Override
     protected void onStart() {
         super.onStart();
